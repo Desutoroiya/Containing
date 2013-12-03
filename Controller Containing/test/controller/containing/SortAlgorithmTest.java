a44830f7572a21df41002a009ebb8eaea40f17a7
Ivar <ivardelelange2003@hotmail.com>
2013-12-03 13:06:17 +0100

    Test2
    SortAlgorithm

---
+++ Controller Containing/test/controller/containing/SortAlgorithmTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.containing;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Iva
+ */
+public class SortAlgorithmTest {
+    
+    public SortAlgorithmTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of arrivePeriod method, of class SortAlgorithm.
+     */
+    @Test
+    public void testArrivePeriod() {
+        System.out.println("arrivePeriod");
+        double timeArrive1 = 0.0;
+        double timeArrive2 = 0.0;
+        SortAlgorithm instance = new SortAlgorithm();
+        instance.arrivePeriod(timeArrive1, timeArrive2);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
