bf23e431d333377b61184312d862046d91befd5a
Fons <fonsbop@hotmail.com>
2013-12-10 19:05:01 +0100

    Code - simulator
    De code die we al hadden weer toegevoegd, vind de modellen nu wel

--- Simulator/src/mygame/Main.java
+++ Simulator/src/mygame/Main.java
@@ -1,36 +1,49 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.renderer.RenderManager;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+
 
 /**
  * test
  * @author normenhansen
  */
-public class Main extends SimpleApplication {
-
-    public static void main(String[] args) {
+public class Main extends SimpleApplication 
+{
+    public static void main(String[] args) 
+    {
         Main app = new Main();
-        app.start();
+        app.start();        
     }
-
+    
     @Override
-    public void simpleInitApp() {
-        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+    public void simpleInitApp()
+    {  
+        flyCam.setMoveSpeed(25);
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
+        /** A white, directional light source */ 
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun); 
 
-        rootNode.attachChild(geom);
-    }
+        AmbientLight ambLight = new AmbientLight();
+        ambLight.setColor(new ColorRGBA(1f, 1f, 0.8f, 0.2f));
+        rootNode.addLight(ambLight);
+        
+        
+        Terrain terrain = new Terrain(assetManager, rootNode); 
+        terrain.initTerrain();
+        viewPort.setBackgroundColor(ColorRGBA.Blue);      
 
+        
+        
+    }    
+    
     @Override
     public void simpleUpdate(float tpf) {
         //TODO: add update code
