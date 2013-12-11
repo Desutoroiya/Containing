8c2f5e2661281e7d32540a7dce38ab90cfd405a5
Johan21 <johanbouwer@hotmail.com>
2013-12-11 13:28:22 +0100

    More Cranes

--- Simulator/src/mygame/CraneController.java
+++ Simulator/src/mygame/CraneController.java
@@ -1,13 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mygame;
 
 /**
  *
  * @author Johan
  */
-public class CraneController {
+public interface CraneController {
     
+
 }
--- Simulator/src/mygame/TruckCrane.java
+++ Simulator/src/mygame/TruckCrane.java
@@ -1,13 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mygame;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 
 /**
  *
  * @author Johan
  */
-public class TruckCrane {
+public class TruckCrane extends Node implements CraneController{
+    
+    public AssetManager assetManager;
+
+    public TruckCrane(AssetManager assetManager){
+        this.assetManager = assetManager;
+    }
+        
+   public void createTruckCrane(){
+       
+              
+       Spatial TCraneBase = assetManager.loadModel("Models/scraneLift.j3o");
+       this.attachChild(TCraneBase);
+       Spatial TCrane = assetManager.loadModel("Models/scraneBase.j3o");
+       this.attachChild(TCrane);
+       
+   }
     
 }
+
