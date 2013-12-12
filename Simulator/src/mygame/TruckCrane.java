e9abb99d19d5b771297b36a315087b89c4568bc7
Johan21 <johanbouwer@hotmail.com>
2013-12-12 09:00:21 +0100

    More Truck Crane

--- Simulator/src/mygame/TruckCrane.java
+++ Simulator/src/mygame/TruckCrane.java
@@ -11,18 +11,25 @@ import com.jme3.scene.Spatial;
 public class TruckCrane extends Node implements CraneController{
     
     public AssetManager assetManager;
+    private Node rootNode;
+    
 
     public TruckCrane(AssetManager assetManager){
         this.assetManager = assetManager;
+        this.rootNode = rootNode;
     }
         
    public void createTruckCrane(){
        
               
-       Spatial TCraneBase = assetManager.loadModel("Models/scraneLift.j3o");
+       Spatial TCraneLift = assetManager.loadModel("Models/scraneLift.j3o");
+       this.attachChild(TCraneLift);
+       Spatial TCraneBase = assetManager.loadModel("Models/scraneBase.j3o");
        this.attachChild(TCraneBase);
-       Spatial TCrane = assetManager.loadModel("Models/scraneBase.j3o");
-       this.attachChild(TCrane);
+       
+        this.setLocalTranslation(69.5f,0,24f);
+        this.rotate(0,1.5707f,0);
+        rootNode.attachChild(this);
        
    }
     
