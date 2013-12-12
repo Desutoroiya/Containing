31173cb7f04e7f6a98c2c1017955ccdd413a76eb
Ivar <ivardelelange2003@hotmail.com>
2013-12-12 13:44:18 +0100

    Train Crane bewegen
    yolo

--- Simulator/src/mygame/TrainCrane.java
+++ Simulator/src/mygame/TrainCrane.java
@@ -15,34 +15,48 @@ import com.jme3.scene.Spatial;
  *
  * @author Ivar
  */
-public class TrainCrane extends Node{
+public class TrainCrane extends Node {
+
     private Node rootNode;
     public AssetManager assetManager;
+    
     private MotionPath trainBase;
-    private MotionEvent motionControl;
-    public boolean loaded = false;
+    private MotionEvent meTrainCrane;
     
+    private Vector3f firstpoint = new Vector3f(0,0,0);
+    private Vector3f secondpoint = new Vector3f(0,5,0);
     
-    public TrainCrane(AssetManager assetManager){
+    private int location;
+    private boolean moving = false;
+    private float baseSpeed = 1.0f;
+    
+    public TrainCrane(AssetManager assetManager) {
         this.assetManager = assetManager;
         this.rootNode = rootNode;
     }
-    
-    public void createTrainCrane(){
-    Spatial TrainCraneLift = assetManager.loadModel("Models/scraneLift.j3o");
-       this.attachChild(TrainCraneLift);
-       Spatial TrainCraneBase = assetManager.loadModel("Models/scraneBase.j3o");
-       this.attachChild(TrainCraneBase);
-       
-       this.setLocalTranslation(69.5f,0,24f);
-       this.rotate(0,1.5707f,0);
-       rootNode.attachChild(this);
-    }
-    
-    public void moveBase(){
+
+    public void createTrainCrane() {
+        Spatial TrainCraneLift = assetManager.loadModel("Models/Storagecrane/scraneLift.j3o");
+        this.attachChild(TrainCraneLift);
+        Spatial TrainCraneBase = assetManager.loadModel("Models/Storagecrane/scraneBase.j3o");
+        this.attachChild(TrainCraneBase);
+
+        this.setLocalTranslation(-63, 0.1f, -28);
+        this.rotate(0, 1.5707f, 0);
         
+    }
+
+    public void moveBase() {
+
         trainBase = new MotionPath();
-        trainBase.addWayPoint(new Vector3f (0,0,0));
-        trainBase.addWayPoint(new Vector3f (0,5,0));
+        trainBase.addWayPoint(new Vector3f(firstpoint));
+        trainBase.addWayPoint(new Vector3f(secondpoint));
+        
+        meTrainCrane = new MotionEvent(this, trainBase);
+        trainBase.setCurveTension(0f);
+        meTrainCrane.setSpeed(baseSpeed);
+        meTrainCrane.play();
+
+
     }
 }
