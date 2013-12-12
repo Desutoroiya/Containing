375b4528560bc46b19258c2831ff072eaa400b52
Johan21 <johanbouwer@hotmail.com>
2013-12-12 15:23:53 +0100

    More Truck again

--- Simulator/src/mygame/Truck.java
+++ Simulator/src/mygame/Truck.java
@@ -17,39 +17,49 @@ import com.jme3.scene.Spatial;
  *
  * @author Enzo van Kessel
  */
-public class Truck extends Node implements VehController  {
+public class Truck extends Node implements VehController {
+
     private AssetManager assetManager;
     private MotionPath path;
     private MotionEvent motionControl;
-    
-    public Truck(AssetManager assetManager){
+
+    public Truck(AssetManager assetManager) {
         this.assetManager = assetManager;
     }
-    
-    public void move(){
-        
-        float distance = 69.5f;
-        boolean full = true;
-        
-        path = new MotionPath();
-        path.addWayPoint(new Vector3f(80f,0f,28f));
 
-        
-        for(int i = 0; i < 19; i++){
-            
-        path.addWayPoint(new Vector3f(distance,0f,28f));
-        
-        distance -=3;
-        
-        path.addWayPoint(new Vector3f((distance),0f,28f));
-        path.addWayPoint(new Vector3f((distance),0f,24f));
-        
+    public void move() {
+
+        float distance = 72.5f;
+        boolean looking = true;
+
+        path = new MotionPath();
+        path.addWayPoint(new Vector3f(80f, 0f, 28f));
+
+
+        if (looking) {
+            for (int i = 0; i < 20; i++) {
+
+                path.addWayPoint(new Vector3f(distance, 0f, 28f));
+                //if () {
+
+                    distance -= 3;
+
+                    path.addWayPoint(new Vector3f((distance), 0f, 28f));
+                    path.addWayPoint(new Vector3f((distance), 0f, 24f));
+                /*} else {
+                    
+                    looking = false;
+                    path.addWayPoint(new Vector3f(distance, 0f, 24f));
+                    break;
+                }*/
+            }
+        } else {
         }
-        
-        motionControl = new MotionEvent(this,path);
+
+        motionControl = new MotionEvent(this, path);
         path.setCurveTension(0f);
         motionControl.setDirectionType(MotionEvent.Direction.PathAndRotation);
-        motionControl.setRotation(new Quaternion().fromAngleNormalAxis(-FastMath.HALF_PI,Vector3f.UNIT_Y));
+        motionControl.setRotation(new Quaternion().fromAngleNormalAxis(-FastMath.HALF_PI, Vector3f.UNIT_Y));
         motionControl.setSpeed(0.5f);
         motionControl.play();
     }
@@ -57,15 +67,15 @@ public class Truck extends Node implements VehController  {
     public boolean Checkloaded() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    public void CreateTruck(){
+
+    public void CreateTruck() {
         Spatial Cart = assetManager.loadModel("Models/Truck/truckCart.j3o");
         Spatial Truck = assetManager.loadModel("Models/Truck/truckMachine.j3o");
         this.attachChild(Cart);
         this.attachChild(Truck);
-        Cart.setLocalTranslation(0,0, 0);
-        Truck.setLocalTranslation(0,0,0);
-        
-        
+        Cart.setLocalTranslation(0, 0, 0);
+        Truck.setLocalTranslation(0, 0, 0);
+
+
     }
 }
