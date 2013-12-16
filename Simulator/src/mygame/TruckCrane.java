9637aeafe1a6927f5cdf26661c3e42b31d090764
reversj <nielsriemersma@hotmail.com>
2013-12-16 13:03:30 +0100

    trucksz

--- Simulator/src/mygame/TruckCrane.java
+++ Simulator/src/mygame/TruckCrane.java
@@ -16,30 +16,31 @@ public class TruckCrane extends Node {
 
     public AssetManager assetManager;
     Node craneLift = new Node();
+    
     private MotionPath mpBase;
     private MotionPath mpHook;
+    private MotionPath mpLift;
+    
     private MotionEvent meTCrane;
     private MotionEvent meTCHook;
+    private MotionEvent meTCLift;
     
     private float X = 0;//69.5f;
     private float Y = 0;
     private float Z = 0;//24f;
     
-    private float baseSpeed = 1.0f;
-    
-    
-    
     Vector3f Position = new Vector3f(X,Y,Z);
+    
+    private float baseSpeed = 1.0f;
 
     public TruckCrane(AssetManager assetManager) {
         this.assetManager = assetManager;
     }
 
-    public void createTruckCrane(int id) {
+    public void createTruckCrane() {
         /*
          * Creates a new cranelift for trucks
          */
-        
         Spatial TCraneLift = assetManager.loadModel("Models/Storagecrane/scraneLift.j3o");
         Spatial TCraneBase = assetManager.loadModel("Models/Storagecrane/scraneBase.j3o");
         Spatial TCraneHook = assetManager.loadModel("Models/Storagecrane/scraneHook.j3o");
@@ -56,28 +57,53 @@ public class TruckCrane extends Node {
     }
 
     public void moveBase(float X, float Y, float Z) {
-        //get id crane
-        mpBase = new MotionPath();
-        mpBase.addWayPoint(new Vector3f(X,Y,Z));
-        mpBase.addWayPoint(new Vector3f(X,Y,Z+2));
+        
+            mpBase = new MotionPath();
+            mpBase.addWayPoint(Position);
+            mpBase.addWayPoint(new Vector3f(Position.x, Position.y, Position.z + 2));
 
-        meTCrane = new MotionEvent(craneLift, mpBase);
-        mpBase.setCurveTension(0f);
-        meTCrane.setSpeed(baseSpeed);
-        meTCrane.play();
+            meTCrane = new MotionEvent(craneLift, mpBase);
+            mpBase.setCurveTension(0f);
+            meTCrane.setSpeed(baseSpeed);
+            meTCrane.play();
         
     }
 
     public void moveHook() {
         Spatial deHook = craneLift.getChild(2);
         mpHook = new MotionPath();
-        mpHook.addWayPoint(new Vector3f(Position.x, Position.y, Position.z));
+        mpHook.addWayPoint(new Vector3f(Position));
         mpHook.addWayPoint(new Vector3f(Position.x, Position.y - 0.5f, Position.z));
         
         meTCHook = new MotionEvent(deHook, mpHook);
         mpHook.setCurveTension(0f);
         meTCHook.setSpeed(baseSpeed);
         meTCHook.play();
+    }
+    
+    public void moveLift(){
+        Spatial deLift = craneLift.getChild(0);
+        mpLift = new MotionPath();
+        mpLift.addWayPoint(new Vector3f(Position));
+        mpLift.addWayPoint(new Vector3f(Position.x, Position.y, Position.z + 0.75f));
+        mpLift.addWayPoint(new Vector3f(Position.x, Position.y, Position.z - 0.75f));
+        mpLift.addWayPoint(new Vector3f(Position));
         
+        meTCLift = new MotionEvent(deLift, mpLift);
+        mpLift.setCurveTension(0f);
+        meTCLift.setSpeed(baseSpeed);
+        meTCLift.play();
+        
+        Spatial deHook = craneLift.getChild(2);
+        mpHook = new MotionPath();
+        mpHook.addWayPoint(new Vector3f(Position));
+        mpHook.addWayPoint(new Vector3f(Position.x, Position.y, Position.z + 0.75f));
+        mpHook.addWayPoint(new Vector3f(Position.x, Position.y, Position.z - 0.75f));
+        mpHook.addWayPoint(new Vector3f(Position));
+        
+        meTCHook = new MotionEvent(deHook, mpHook);
+        mpHook.setCurveTension(0f);
+        meTCHook.setSpeed(baseSpeed);
+        meTCHook.play();
     }
 }
