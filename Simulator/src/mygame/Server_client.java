3f0dd11f41782b5e3b3015e3e5d672bec1aa1a24
Johan21 <johanbouwer@hotmail.com>
2014-01-08 09:26:16 +0100

    Meer Sockets

--- Simulator/src/mygame/Server_client.java
+++ Simulator/src/mygame/Server_client.java
@@ -5,34 +5,86 @@
 package mygame;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Fons
  */
-public class Server_client 
+public class Server_client implements Runnable
 {
-    public static void main(String[] args)
-    {
-        try
-        {
-            Socket sock = new Socket("localhost", 9090);
-            PrintStream pr = new PrintStream(sock.getOutputStream());
+    
+    private static Socket clientSocket;
+    private static PrintStream os;
+    private static DataInputStream is;
+    private static BufferedReader inputLine;
+    
+    private static boolean open = true;
+    private static String hostname;
+    private static int port;
+    private static String sendMessage;
+    private static String recievedMessage;
+    public static List<String> log = new ArrayList<String>();
+    
+    public static void main(String[] args){
+        
+        hostname = "localhost";
+        port = 6060;
+        try {
             
-            System.out.print("Enter something: ");
+            clientSocket = new Socket(hostname, port);
+            inputLine = new BufferedReader(new InputStreamReader(System.in));
+            os = new PrintStream(clientSocket.getOutputStream());
+            is = new DataInputStream(clientSocket.getInputStream());
             
-            InputStreamReader rd = new InputStreamReader(System.in);
-            BufferedReader ed = new BufferedReader(rd);
-            
-            String temp = ed.readLine();
-            pr.println(temp);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        catch(Exception e)
-        {
-
+        
+        if(clientSocket != null && os != null && is != null){
+            
+            try{
+                
+               new Thread(new Server_client()).start();
+               
+               while(open){
+                   
+                   os.println(inputLine.readLine());
+                   
+               }
+              os.close();
+              is.close();
+              clientSocket.close();
+            }catch(IOException ex){
+                ex.printStackTrace();
+            }
         }
+    }
+    
+    public void run(){
+        
+        try{
+            
+            while(true){
+                
+               recievedMessage = is.readLine();
+               System.out.println(recievedMessage);
+               
+               log.add(recievedMessage);
+               
+               //System.out.println(log);
+      
+            }
+            
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+        
     }
 }
